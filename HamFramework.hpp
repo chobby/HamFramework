22d8855307590943e9eee41823705d97da638e92
chobby <chobby75@gmail.com>
2016-06-30 11:00:59 +0900

    add hpp

---
+++ HamFramework.hpp
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the HamFramework for Siv3D.
+//
+//	Copyright (C) 2014-2016 Hamukun
+//	Copyright (C) 2014-2016 Ryo Suzuki
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+
+# ifndef SIV3D_INCLUDED
+#  error <Siv3D.hpp> をインクルードしてください | #include <Siv3D.hpp> is required
+# endif
+
+# include "HamFramework/SceneManager.hpp"
+# include "HamFramework/LayerManager.hpp"
+# include "HamFramework/SceneManager.hpp"
+# include "HamFramework/SizableWindowManager2D.hpp"
+# include "HamFramework/CameraManager2D.hpp"
+# include "HamFramework/Box2DManager.hpp"
+
+# ifndef NO_HAM_USING
+	using namespace ham;
+# endif
